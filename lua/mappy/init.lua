79e0d85f737b2392153d46b1609744b5f0dc75d5
shift-d <shiftkeydeveloper@gmail.com>
2022-01-10 20:14:40 +0300

    feat: multiple modes, restructure

--- lua/mappy/init.lua
+++ lua/mappy/init.lua
@@ -23,14 +23,34 @@ local function walk(map_table, prev)
 	return outline
 end
 
+---Generate reusable mapper function. Reused with different modes.
+---@param api function
+---@param lhs string
+---@param rhs string | function
+---@param opts table
+---@return function
+local function gen_mapper(api, lhs, rhs, opts)
+    return function(mode)
+        api(mode, lhs, rhs, opts)
+    end
+end
+
 ---Set mappings using nvim_set_keymap api
 ---@param maps table
 ---@param options table
 mappy.stable = function (maps, options)
-    for mode, mode_maps in pairs(maps) do
-        local outline = walk(mode_maps)
-        for lhs, rhs in pairs(outline) do
-            vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+    options = options or {}
+    local outline = walk(maps)
+    for lhs, rhs in pairs(outline) do
+        local map = gen_mapper(vim.api.nvim_set_keymap, lhs, rhs, options.map)
+        if type(options.mode) == "table" then
+            for _, modechar in pairs(options.mode) do
+                map(modechar)
+            end
+        elseif options.mode == nil then
+            map("n")
+        else
+            map(options.mode)
         end
     end
 end
@@ -39,12 +59,20 @@ end
 ---@param maps table
 ---@param options table
 mappy.nightly = function(maps, options)
-	for mode, mode_maps in pairs(maps) do
-		local outline = walk(mode_maps)
-		for lhs, rhs in pairs(outline) do
-			vim.keymap.set(mode, lhs, rhs, options)
-		end
-	end
+    options = options or {}
+    local outline = walk(maps)
+    for lhs, rhs in pairs(outline) do
+        local map = gen_mapper(vim.keymap.set, lhs, rhs, options.map)
+        if type(options.mode) == "table" then
+            for _, modechar in pairs(options.mode) do
+                map(modechar)
+            end
+        elseif options.mode == nil then
+            map("n")
+        else
+            map(options.mode)
+        end
+    end
 end
 
 return mappy
