b59ad510a8f7290e1b014cc3b22fb64c03ec3538
shift-d <shiftkeydeveloper@gmail.com>
2022-01-11 18:36:15 +0300

    feat(map): event-based mapping

--- lua/mappy/init.lua
+++ lua/mappy/init.lua
@@ -42,6 +42,14 @@ mappy.stable = function (maps, options)
     options = options or {}
     local outline = walk(maps)
     for lhs, rhs in pairs(outline) do
+        if type(rhs) ~= "string" then
+            if type(rhs) == "function" then
+                vim.notify("You can map a lua function only if you are using nightly api!", "error", {title="mappy.nvim"})
+            else
+                vim.notify("You can only map a string in stable mode", "error", {title="mappy.nvim"})
+            end
+            return
+        end
         local map = gen_mapper(vim.api.nvim_set_keymap, lhs, rhs, options.map)
         if type(options.mode) == "table" then
             for _, modechar in pairs(options.mode) do
@@ -59,9 +67,13 @@ end
 ---@param maps table
 ---@param options table
 mappy.nightly = function(maps, options)
+    vim.notify("Exec")
     options = options or {}
     local outline = walk(maps)
     for lhs, rhs in pairs(outline) do
+        if type(rhs) ~= "function" or type(rhs) ~= "string" then
+            vim.notify("You can map only a string or a function as rhs!", "error", {title="mappy.nvim"})
+        end
         local map = gen_mapper(vim.keymap.set, lhs, rhs, options.map)
         if options.mode == nil then
             map("n")
@@ -77,11 +89,32 @@ mappy.link = function(maps)
     local present, wk = pcall(require, "which-key")
     if not present then
         vim.notify("folke/which-key.nvim could not be loaded, aborting linking", "error", {title = "mappy.nvim"})
+        return
     end
     local links = walk(maps)
     for mapping, description in pairs(links) do
         wk.register({ [mapping] = { name = description } })
     end
+end
+
+-- TODO: clean autocmds on the mappy setup
+
+---Automap only if event if triggered
+---@param maps table
+---@param event string
+---@param version string
+---@param opts table
+---@param storage string
+mappy.event = function(maps, event, version, storage)
+    if storage == nil then
+        vim.notify("Specify global variable name where mappings will be stored","error", {title="mappy.nvim"})
+        return
+    end
+    opts = opts or {}
+    vim.g[storage] = maps
+    vim.cmd("augroup mappy")
+    vim.cmd("au "..event.." lua require('mappy')."..version.."(vim.g."..storage..")")
+    vim.cmd("augroup END")
 end
 
 return mappy
